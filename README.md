2c0bb4461fc7019200db3d95c29b6a5b59cdbfc1
cyc-987 <2516803593@qq.com>
2024-08-03 21:03:20 +0800

    add: main.py utils.py; notes: 完成数据读入和预处理

---
+++ README.md
@@ -0,0 +1,7 @@
+# 使用模拟退火计算旅行商问题
+
+# 构成
+- `utils.py`：预处理数据函数
+
+# 数据
+数据来源于此处：[National Traveling Salesman Problems](https://www.math.uwaterloo.ca/tsp/world/countries.html)
---
+++ utils.py
@@ -0,0 +1,53 @@
+import numpy as np
+import math
+
+class tspProblem():
+    def __init__(self, filepath):
+        self.__checkTSPformat(filepath)
+        self.__getMetaData(filepath)
+        self.__convertToDistanceMatrix()
+    
+    #private funcs
+    def __checkTSPformat(self, filepath):
+        assert (filepath[-4:] == ".tsp")
+    
+    def __getMetaData(self, filepath):
+        tsp = open(filepath, mode='r')
+        #get numberof nodes
+        line = tsp.readline()
+        while(line[:9] != 'DIMENSION'): 
+            line = tsp.readline()
+        sp = line.split()
+        nodes = [node for node in sp if node.isdigit()]
+        numOfNodes = int(nodes[0])
+        
+        #get cordinate data
+        while(line != 'NODE_COORD_SECTION\n'):
+            line = tsp.readline()
+        cordinates = np.zeros((numOfNodes, 2))
+        for i in range(0, numOfNodes):
+            line = tsp.readline()
+            single = np.fromstring(line, dtype=float, sep=' ')
+            cordinates[i,:] = single[1:3]
+        
+        #end
+        tsp.close()
+        self.numOfNodes = numOfNodes
+        self.cordinates = cordinates
+    
+    def __convertToDistanceMatrix(self):
+        # print(self.numOfNodes)
+        # print(self.cordinates)
+        distanceMatrix = np.zeros((self.numOfNodes, self.numOfNodes))
+        for i in range(0, self.numOfNodes):
+            for j in range(i, self.numOfNodes):
+                distanceMatrix[i,j] = self.__distance(i,j)
+                distanceMatrix[j,i] = distanceMatrix[i,j]
+        
+        #end
+        self.distMatrix = distanceMatrix
+    
+    def __distance(self, i, j):
+        dx = self.cordinates[i, 0] - self.cordinates[j, 0]
+        dy = self.cordinates[i, 1] - self.cordinates[j, 1]
+        return math.sqrt(dx**2 + dy**2)
